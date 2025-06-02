09e3e0a03822ab22d3a07dc3a8fd3bc6c6574f2d
Daulet <Daulet>
2025-06-02 06:20:18 +0500

    add

--- src/features/SignUp/model/services/signUpClick/signUpClick.ts
+++ src/features/SignUp/model/services/signUpClick/signUpClick.ts
@@ -3,19 +3,18 @@ import { ThunkConfig } from "app/Providers/StoreProvider"
 import { signUp } from "../signUp/signUp"
 import { signUpActions } from "../../slice/signUpSlice"
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime"
+import { getUserAuthData } from "entities/User"
 
 export const signUpClick = createAsyncThunk<void, AppRouterInstance, ThunkConfig<string>>(
     'signUp/signUpClick',
     //@ts-ignore
     async (router, thunkAPI) => {
-        const { dispatch } = thunkAPI
+        const { dispatch, getState } = thunkAPI
+        const user = getUserAuthData(getState())
         
         try {
             const result = await dispatch(signUp()).unwrap()
         
-            if (result) {
-                router.push('/profile')
-            }
                         
             } catch(e) {
                 console.log(e)
